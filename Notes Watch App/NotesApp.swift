8709d5403f318415695cfa414ecb31293dfdc24d
Himindu <himindukularathne@gmail.com>
2024-02-04 08:05:57 +0530

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Himindu Kularathne on 2024-02-04.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
