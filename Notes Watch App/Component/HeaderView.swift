2c5fc2d488fca014e40911979f905bccb1b3b110
Himindu <himindukularathne@gmail.com>
2024-02-06 16:45:53 +0530

    credits view , detail view and settings view is created

---
+++ Notes Watch App/Component/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Notes Watch App
+//
+//  Created by Himindu Kularathne on 2024-02-06.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
---
+++ Notes Watch App/View/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Notes Watch App
+//
+//  Created by Himindu Kularathne on 2024-02-06.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
