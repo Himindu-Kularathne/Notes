bff82b498a7457e5a79fa750a159e2d300666757
Himindu <himindukularathne@gmail.com>
2024-02-06 10:52:50 +0530

    detail view file and all the funtionalities are created

---
+++ Notes Watch App/Model/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  Notes Watch App
+//
+//  Created by Himindu Kularathne on 2024-02-05.
+//
+
+import Foundation
---
+++ Notes Watch App/View/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Notes Watch App
+//
+//  Created by Himindu Kularathne on 2024-02-06.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
